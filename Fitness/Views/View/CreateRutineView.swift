4f57e51daef3e6908ba4e012e9b7f4769170093b
FedericoHerreraa <fede.juan.herrera@gmail.com>
2024-10-30 09:48:23 -0300

    Changing title of each page to have the navigationTItle instead of a single title

---
+++ Fitness/Views/View/CreateRutineView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateRutineView.swift
+//  Fitness
+//
+//  Created by Federico Herrera  on 29/10/2024.
+//
+
+import SwiftUI
+
+struct CreateRutineView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateRutineView()
+}
---
+++ Fitness/Views/View/StepsFollowView.swift
@@ -0,0 +1,18 @@
+//
+//  StepsFollowView.swift
+//  Fitness
+//
+//  Created by Federico Herrera  on 29/10/2024.
+//
+
+import SwiftUI
+
+struct StepsFollowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StepsFollowView()
+}
